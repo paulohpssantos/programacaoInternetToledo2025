c03228175b5214a503adb9896de5be800694d656
Paulo Henrique Pereira dos Santos <84387138+paulohpssantos@users.noreply.github.com>
2025-04-24 19:21:32 -0300

    atualizado

--- 2Bimestre/exemploJavaScript/script.js
+++ 2Bimestre/exemploJavaScript/script.js
@@ -1,3 +1,18 @@
 let titulo = document.querySelector("#titulo");   
 let campoTexto = document.querySelector("#campoTexto");
 let btTrocarTexto = document.querySelector("#btTrocarTexto");
+
+function alterarTexto(){
+    //Retirando o valor digitado no input 
+    //e jogando na variável
+    let textoDigitado = campoTexto.value;
+
+    //Atribuindo ao elemento titulo o texto que foi digitado
+    //no input
+    titulo.textContent = textoDigitado;
+}
+
+//Atribuindo uma ação de clicar no botão
+btTrocarTexto.onclick = function(){
+    alterarTexto();
+}
